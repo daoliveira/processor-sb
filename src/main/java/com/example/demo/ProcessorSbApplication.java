97ac11ffbb3dc29f192f906dcb43b05bef1b9010
daoliveira <daoliveira@gmail.com>
2020-05-06 16:58:31 -0400

    INI-001 Adding Spring Boot

---
+++ src/main/java/com/example/demo/ProcessorSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessorSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessorSbApplication.class, args);
+	}
+
+}
